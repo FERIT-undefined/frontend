b305765cacf7d3611f708e8a94c91db11a6368bd
Josip Papez <papezjosip@gmail.com>
2020-11-29 14:36:43 +0100

    set up project, added eslint and redux, set up redux and redux persist

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App" />
+    </BrowserRouter>
+  );
+}
+
+export default App;
