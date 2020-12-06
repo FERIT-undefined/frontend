8d80bc6f0d08d526b709f78f83e55ed697cf1c9f
Josip Papez <papezjosip@gmail.com>
2020-12-06 12:00:18 +0100

    updated navigation for different roles

--- src/components/Navigation/NavbarContainer.jsx
+++ src/components/Navigation/NavbarContainer.jsx
@@ -3,22 +3,38 @@ import { Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function NavbarContainer(props) {
+  console.log(props.user);
   return (
     <Navbar bg="light" expand="lg">
       <Navbar.Brand>React-Bootstrap</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link>
-            <Link to="/zaposlenici">Zaposlenici</Link>
-          </Nav.Link>
-          <Nav.Link>
-            <Link to="/meni">Meni</Link>
-          </Nav.Link>
-          <Nav.Link>
-            <Link to="/promet">Promet</Link>
-          </Nav.Link>
-        </Nav>
+        {props.user.role === "Admin" &&
+          <Nav className="mr-auto">
+            <Nav.Link>
+              <Link to="/">Zaposlenici</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/meni">Meni</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/promet">Promet</Link>
+            </Nav.Link>
+          </Nav>
+        }
+        {props.user.role === "User" &&
+          <Nav className="mr-auto">
+            <Nav.Link>
+              <Link to="/">Narudžbe</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/meni">Meni</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/stolovi">Stolovi</Link>
+            </Nav.Link>
+          </Nav>
+        }
       </Navbar.Collapse>
       <Navbar.Collapse className="justify-content-end">
         <Navbar.Text>
