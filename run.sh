bd89715295528fc257f6dc609a912184fbc54d20
Matej Arlović <matej.arlovic@gmail.com>
2021-01-22 08:50:22 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo screen -S frontend -X quit;
-sleep 1;
-sudo screen -dmS frontend -L bash -c ' sleep 2; cd /var/lib/jenkins/workspace/brzaklopa_frontend; sleep 1; npm run start:prod'
